6fb9ef39a3d8ea279efa334e1b207df14e407eda
Tristan Rice <rice@fn.lc>
2018-01-21 21:17:19 -0800

    react-navigation

--- DNDAssistant/App.js
+++ DNDAssistant/App.js
@@ -1,21 +1,56 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Platform, StyleSheet, Text, View, StatusBar, ToolbarAndroid } from 'react-native'
 import SpellBook from './SpellBook.js'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <SpellBook>
-        </SpellBook>
+      <View>
+        <StatusBar
+          style={styles.statusbar}
+          backgroundColor="blue"
+          barStyle="light-content"
+        />
+        <View style={styles.container}>
+          <ToolbarAndroid
+            style={styles.toolbar}
+            title="DND Assistant"
+            //actions={[{title: 'Settings', icon: require('./icon_settings.png'), show: 'always'}]}
+            onActionSelected={this.onActionSelected}
+          />
+          <SpellBook>
+          </SpellBook>
+        </View>
       </View>
     )
+  }
+
+  onActionSelected (position) {
+    /*
+    if (position === 0) { // index of 'Settings'
+      showSettings();
+    }
+    */
   }
 }
 
 const styles = StyleSheet.create({
+  outer: {
+    flex: 1
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+  },
+  statusbar: {
+  },
+  toolbar: {
+    backgroundColor: '#2196F3',
+    height: 56,
+    ...Platform.select({
+      android: {
+        marginTop: StatusBar.currentHeight
+      }
+    })
   }
 })
--- DNDAssistant/SpellBook.js
+++ DNDAssistant/SpellBook.js
@@ -1,15 +1,39 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import spells from '../dnd-spells/spells.json'
+import { StyleSheet, Text, View, FlatList, TouchableNativeFeedback } from 'react-native'
+import spells from './dnd-spells/spells.json'
 
-export default class SpellBook extends React.Component {
+class SpellItem extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>SpellBook ... </Text>
-        <Text>blahhh</Text>
+      <View>
+        <TouchableNativeFeedback
+            onPress={this._onPressButton}
+            background={TouchableNativeFeedback.SelectableBackground()}>
+          <View style={styles.item}>
+            <Text>{this.props.spell.name}</Text>
+            <Text>{this.props.spell.class}</Text>
+          </View>
+        </TouchableNativeFeedback>
       </View>
     )
+  }
+}
+
+export default class SpellBook extends React.Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={spells}
+          keyExtractor={this.spellExtractor}
+          renderItem={({item}) => <SpellItem spell={item}/>}
+        />
+      </View>
+    )
+  }
+
+  spellExtractor (spell) {
+    return spell.name
   }
 }
 
@@ -17,5 +41,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
+  },
+  item: {
+    flex: 1,
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee'
   }
 })

194c308c1f36554a40d9bc39681129c1f112c878
Tristan Rice <rice@fn.lc>
2018-01-22 12:32:47 -0800

    ejected

--- App.js
+++ App.js
@@ -2,42 +2,117 @@ import React from 'react'
 import { Platform, StyleSheet, Text, View, StatusBar, ToolbarAndroid } from 'react-native'
 import SpellBook from './SpellBook.js'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
+import { Navigation } from 'react-native-navigation'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 export default class App extends React.Component {
-  render() {
+	render () {
+		return (
+			<View></View>
+    )
+  }
+}
+/*
+  render () {
     return (
       <View style={styles.outer}>
         <StatusBar
           style={styles.statusbar}
-          backgroundColor="blue"
-          barStyle="light-content"
+          backgroundColor='#0384C3'
+          barStyle='light-content'
         />
         <View style={styles.container}>
           <ToolbarAndroid
             style={styles.toolbar}
-            title="DND Assistant"
-            //actions={[{title: 'Settings', icon: require('./icon_settings.png'), show: 'always'}]}
+            title="Tristan's DND Assistant"
             onActionSelected={this.onActionSelected}
+            titleColor='white'
           />
-          <ScrollableTabView>
-            <View tabLabel="Character">
-              <Text>Character</Text>
-            </View>
-            <SpellBook tabLabel="Spells">
+          <ScrollableTabView
+            tabBarBackgroundColor='#039be5'
+            tabBarActiveTextColor='white'
+            tabBarInactiveTextColor='white'>
+
+            <SpellBook tabLabel='Spells'>
             </SpellBook>
           </ScrollableTabView>
         </View>
       </View>
     )
   }
+}
+*/
 
-  onActionSelected (position) {
-    /*
-    if (position === 0) { // index of 'Settings'
-      showSettings();
-    }
-    */
+class Character extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>Character</Text>
+      </View>
+    )
   }
+}
+
+// define your suffixes by yourself..
+// here we use active, big, small, very-big..
+const replaceSuffixPattern = /--(active|big|small|very-big)/g;
+const icons = {
+  "md-person": [30, "#bbb"],
+  "md-book": [30, "#bbb"]
+}
+
+const defaultIconProvider = Ionicons
+
+let iconsMap = {};
+let iconsLoaded = new Promise((resolve, reject) => {
+  new Promise.all(
+    Object.keys(icons).map(iconName => {
+      const Provider = icons[iconName][2] || defaultIconProvider
+      const name = iconName.replace(replaceSuffixPattern, '')
+      return Provider.getImageSource(
+        name,
+        icons[iconName][0],
+        icons[iconName][1]
+      )
+    })
+  ).then(sources => {
+    Object.keys(icons)
+      .forEach((iconName, idx) => iconsMap[iconName] = sources[idx])
+
+    // Call resolve (and we are done)
+    resolve(true)
+  })
+})
+
+iconsLoaded.then(() => {
+  startApp()
+})
+
+function startApp () {
+  // we can be sure, that iconsMap has the icons now
+  // iconsMap should have all the references to sources available now
+  // <Image source={iconsMap['ios-person--active--big']} />
+  // Or use them with react-native-navigation
+
+  Navigation.registerComponent('CharacterScreen', () => Character)
+  Navigation.registerComponent('SpellBook', () => SpellBook)
+
+  Navigation.startTabBasedApp({
+    tabs: [
+      {
+        label: 'Character',
+        screen: 'CharacterScreen', // this is a registered name for a screen
+        icon: iconsMap['md-person'],
+        title: 'Character'
+      },
+      {
+        label: 'Spells',
+        screen: 'SpellBook',
+        icon: iconsMap['md-book'],
+        title: 'Spells'
+      }
+    ]
+  })
 }
 
 const styles = StyleSheet.create({
@@ -51,7 +126,7 @@ const styles = StyleSheet.create({
   statusbar: {
   },
   toolbar: {
-    backgroundColor: '#2196F3',
+    backgroundColor: '#039be5',
     height: 56,
     ...Platform.select({
       android: {
--- SpellBook.js
+++ SpellBook.js
@@ -1,21 +1,147 @@
 import React from 'react'
 import { StyleSheet, Text, View, FlatList, TouchableNativeFeedback } from 'react-native'
 import spells from './dnd-spells/spells.json'
+import ActionButton from 'react-native-action-button'
+import HTMLView from 'react-native-htmlview'
+
+class Quote extends React.Component {
+  render () {
+    return (
+      <View style={{
+        borderLeftWidth: 2,
+        borderLeftColor: '#039be5',
+        paddingLeft: 5,
+        marginTop: 5,
+        marginBottom: 5
+      }}>
+        {this.props.children}
+      </View>
+    )
+  }
+}
 
 class SpellItem extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      showDetail: false
+    }
+  }
+
   render () {
     return (
       <View>
         <TouchableNativeFeedback
-            onPress={this._onPressButton}
+            onPress={this._onPress.bind(this)}
             background={TouchableNativeFeedback.SelectableBackground()}>
           <View style={styles.item}>
-            <Text>{this.props.spell.name}</Text>
-            <Text>{this.props.spell.class}</Text>
+            <View style={styles.row}>
+              <View>
+                <Text style={styles.bold}>{this.props.spell.name}</Text>
+                <Text>{this.props.spell.class}</Text>
+                <Text>{this.props.spell.school}</Text>
+              </View>
+              <View style={styles.right}>
+                <Text>{this.props.spell.level}</Text>
+                <Text>{this.props.spell.page}</Text>
+                <Text>{this.props.spell.casting_time}, {this.props.spell.components}</Text>
+              </View>
+            </View>
+
+            {this.detail()}
           </View>
         </TouchableNativeFeedback>
       </View>
     )
+  }
+
+  detail () {
+    if (!this.state.showDetail) {
+      return
+    }
+
+    return (
+      <View style={styles.detail}>
+        <View style={styles.row}>
+          <View style={styles.grow}>
+            <Text style={styles.bold}>Duration</Text>
+            <Text>{this.props.spell.duration}</Text>
+          </View>
+
+          <View style={styles.grow}>
+            <Text style={styles.bold}>Range</Text>
+            <Text>{this.props.spell.range}</Text>
+          </View>
+
+          {this.props.spell.material ? <View style={styles.grow}>
+            <Text style={styles.bold}>Material</Text>
+            <Text>{this.props.spell.material}</Text>
+            </View> : ''}
+        </View>
+
+        <View style={styles.row}>
+          <View style={styles.grow}>
+            <Text style={styles.bold}>Concentration</Text>
+            <Text>{this.props.spell.concentration}</Text>
+          </View>
+
+          <View style={styles.grow}>
+            <Text style={styles.bold}>Ritual</Text>
+            <Text>{this.props.spell.ritual}</Text>
+          </View>
+        </View>
+
+
+        <Text style={styles.bold}>Description</Text>
+        <Quote>
+          <HTMLView
+            value={this.props.spell.desc}
+          />
+        </Quote>
+
+        {this.higherLevel()}
+      </View>
+    )
+  }
+
+  higherLevel () {
+    if (!this.props.spell.higher_level) {
+      return
+    }
+
+    return (
+      <View>
+        <Text style={styles.bold}>Higher Level</Text>
+        <Quote>
+          <HTMLView
+            value={this.props.spell.higher_level}
+          />
+        </Quote>
+      </View>
+    )
+  }
+
+  _onPress () {
+    this.setState(prev => {
+      return { showDetail: !prev.showDetail }
+    })
+  }
+}
+
+class SpellList extends React.Component {
+  render () {
+    return (
+      <FlatList
+        data={spells}
+        keyExtractor={this.spellExtractor}
+        renderItem={({item}) => <SpellItem spell={item}/>}
+      />
+    )
+  }
+
+  spellExtractor (spell) {
+    return spell.name
   }
 }
 
@@ -23,17 +149,14 @@ export default class SpellBook extends React.Component {
   render () {
     return (
       <View style={styles.container}>
-        <FlatList
-          data={spells}
-          keyExtractor={this.spellExtractor}
-          renderItem={({item}) => <SpellItem spell={item}/>}
+        <SpellList />
+
+        <ActionButton
+          buttonColor="rgba(231,76,60,1)"
+          onPress={() => { console.log("hi")}}
         />
       </View>
     )
-  }
-
-  spellExtractor (spell) {
-    return spell.name
   }
 }
 
@@ -47,5 +170,22 @@ const styles = StyleSheet.create({
     padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#eee'
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  right: {
+    alignItems: 'flex-end'
+  },
+  bold: {
+    fontWeight: 'bold'
+  },
+  detail: {
+    marginTop: 10
+  },
+  grow: {
+    flexGrow: 10000,
   }
 })

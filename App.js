39d83b11bab374507f50a8806379b42f9d444513
Tristan Rice <rice@fn.lc>
2018-02-05 12:12:13 -0800

    App: show (DEBUG) label if debug build

--- App.js
+++ App.js
@@ -20,6 +20,88 @@ export const start = () => {
     default: []
   })
 
+  const tabs = [
+    {
+      label: 'Character',
+      screen: 'dnd.CharacterScreen', // this is a registered name for a screen
+      icon: iconsMap['md-person'],
+      title: 'Character',
+      navigatorButtons: {
+        rightButtons: [
+          ...rightButtons,
+          {
+            id: 'share',
+            title: 'Sharing Settings',
+            icon: iconsMap['md-share']
+          }
+        ],
+        leftButtons: leftButtons
+      }
+    },
+    {
+      label: 'Gear',
+      screen: 'dnd.GearScreen', // this is a registered name for a screen
+      icon: iconsMap['md-basket'],
+      title: 'Gear',
+      navigatorButtons: {
+        fab: {
+          collapsedId: 'add',
+          collapsedIcon: iconsMap['md-add'],
+          collapsedIconColor: colors.textPrimary,
+          backgroundColor: colors.accent,
+          animated: true
+        },
+        rightButtons: rightButtons,
+        leftButtons: leftButtons
+      }
+    },
+    {
+      label: 'Spells',
+      screen: 'dnd.KnownSpellsScreen',
+      icon: iconsMap['md-book'],
+      title: 'Spells',
+      navigatorButtons: {
+        fab: {
+          collapsedId: 'add',
+          collapsedIcon: iconsMap['md-add'],
+          collapsedIconColor: colors.textPrimary,
+          backgroundColor: colors.accent,
+          animated: true
+        },
+        rightButtons: [
+          ...rightButtons,
+          {
+            id: 'slots',
+            title: 'Slots',
+            icon: iconsMap['md-settings']
+          },
+          {
+            id: 'wildMagic',
+            title: 'Wild Magic',
+            icon: iconsMap['fire']
+          },
+          {
+            id: 'resetSlots',
+            title: 'Reset Slots',
+            icon: iconsMap['clear-all']
+          },
+          {
+            id: 'filter',
+            title: 'Filter',
+            icon: iconsMap['md-search']
+          }
+        ],
+        leftButtons: leftButtons
+      }
+    }
+  ]
+
+  if (__DEV__) {
+    tabs.forEach(tab => {
+      tab.title = tab.title + ' (DEBUG)'
+    })
+  }
+
   Navigation.startTabBasedApp({
     appStyle: {
       navBarBackgroundColor: colors.primary,
@@ -30,81 +112,7 @@ export const start = () => {
       tabBarButtonColor: colors.secondaryText,
       tabBarSelectedButtonColor: colors.primary
     },
-    tabs: [
-      {
-        label: 'Character',
-        screen: 'dnd.CharacterScreen', // this is a registered name for a screen
-        icon: iconsMap['md-person'],
-        title: 'Character',
-        navigatorButtons: {
-          rightButtons: [
-            ...rightButtons,
-            {
-              id: 'share',
-              title: 'Sharing Settings',
-              icon: iconsMap['md-share']
-            }
-          ],
-          leftButtons: leftButtons
-        }
-      },
-      {
-        label: 'Gear',
-        screen: 'dnd.GearScreen', // this is a registered name for a screen
-        icon: iconsMap['md-basket'],
-        title: 'Gear',
-        navigatorButtons: {
-          fab: {
-            collapsedId: 'add',
-            collapsedIcon: iconsMap['md-add'],
-            collapsedIconColor: colors.textPrimary,
-            backgroundColor: colors.accent,
-            animated: true
-          },
-          rightButtons: rightButtons,
-          leftButtons: leftButtons
-        }
-      },
-      {
-        label: 'Spells',
-        screen: 'dnd.KnownSpellsScreen',
-        icon: iconsMap['md-book'],
-        title: 'Spells',
-        navigatorButtons: {
-          fab: {
-            collapsedId: 'add',
-            collapsedIcon: iconsMap['md-add'],
-            collapsedIconColor: colors.textPrimary,
-            backgroundColor: colors.accent,
-            animated: true
-          },
-          rightButtons: [
-            ...rightButtons,
-            {
-              id: 'slots',
-              title: 'Slots',
-              icon: iconsMap['md-settings']
-            },
-            {
-              id: 'wildMagic',
-              title: 'Wild Magic',
-              icon: iconsMap['fire']
-            },
-            {
-              id: 'resetSlots',
-              title: 'Reset Slots',
-              icon: iconsMap['clear-all']
-            },
-            {
-              id: 'filter',
-              title: 'Filter',
-              icon: iconsMap['md-search']
-            }
-          ],
-          leftButtons: leftButtons
-        }
-      }
-    ],
+    tabs: tabs,
     drawer: {
       left: {
         screen: 'dnd.CharacterMenu',

21fe4050324716bccbd5b12ba7a3a79864218352
Tristan Rice <rice@fn.lc>
2018-02-26 15:13:04 -0800

    DieRoll.test: added modifier test

--- DieRoll.test.js
+++ DieRoll.test.js
@@ -11,4 +11,18 @@ it('rolls fate correctly', () => {
   for (let mod = -4; mod <= 4; mod++) {
     expect(rolls[mod]).toBeGreaterThan(0)
   }
+  expect(Object.keys(rolls).length).toBe(9)
+})
+
+it('rolls fate correctly with modifier', () => {
+  const rolls = {}
+  for (let i = 0; i < 10000; i++) {
+    const roll = rollFate({modifier: 5})
+    rolls[roll] = (rolls[roll] || 0) + 1
+  }
+
+  for (let mod = 1; mod <= 9; mod++) {
+    expect(rolls[mod]).toBeGreaterThan(0)
+  }
+  expect(Object.keys(rolls).length).toBe(9)
 })

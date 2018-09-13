cf2db5eb7c329f5e0ed3ed25e118fb50b532ecb5
Tristan Rice <rice@fn.lc>
2018-09-13 12:31:33 -0700

    fixed haul config

--- haul.config.js
+++ haul.config.js
@@ -1,9 +1,12 @@
 import { createWebpackConfig } from "haul";
 
 export default {
-  webpack: createWebpackConfig((options) => ({
+  webpack: createWebpackConfig((options) => {
     console.log('haul config running')
     options.minify = false
-    entry: `./index.js`
-  }))
+
+    return {
+      entry: `./index.js`
+    }
+  })
 };

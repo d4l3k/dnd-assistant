450333ed8cf010dca587d659eef154e418ecf3fb
Tristan Rice <rice@fn.lc>
2022-11-26 15:05:02 -0800

    webpack.prod: build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,32 +1,24 @@
-const merge = require('webpack-merge')
+const {merge} = require('webpack-merge')
 const common = require('./webpack.common.js')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const path = require('path')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = merge(common, {
+  mode: 'production',
   devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
       __DEV__: 'false'
     }),
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
-    new CleanWebpackPlugin(['docs']),
     new HtmlWebpackPlugin({
       title: 'Tristan\'s DND Assistant'
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    new CopyWebpackPlugin([
-      { from: 'static' }
-    ]),
     new FaviconsWebpackPlugin(path.resolve(__dirname, 'media/logo-icon.png'))
   ],
   output: {

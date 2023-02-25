d6ef8bbeade2fe85857ccb6a0805646c02fa3bc6
Tristan Rice <rice@fn.lc>
2023-02-25 15:18:52 -0800

    spells: added egtw spells

--- data/index.js
+++ data/index.js
@@ -1,6 +1,7 @@
 import dataSpells from './spellData.json'
 import dataDisciplines from './disciplines.json'
 import dataTCOE from './tcoe.json'
+import dataEGTW from './egtw.json'
 
 const spells = dataSpells.concat(dataDisciplines)
 
@@ -17,35 +18,40 @@ const levelStrs = [
   "9th-level",
 ]
 
-for (const key in dataTCOE) {
-  if (!key.startsWith("Spells.")) {
-    continue
-  }
-  const {
-    Name, CastingTime, Classes, Components, Description, Duration, Level,
-    Range, Ritual, School
-  } = dataTCOE[key]
+function processDict (data, page) {
+  for (const key in data) {
+    if (!key.startsWith("Spells.")) {
+      continue
+    }
+    const {
+      Name, CastingTime, Classes, Components, Description, Duration, Level,
+      Range, Ritual, School
+    } = data[key]
 
-  const cantrip = Level == 0
-  const comp_bits = Components.split(" (")
-  const components = comp_bits[0]
-  const materials = comp_bits.length < 2 ? "" : comp_bits[1].split(")")[0]
-  const concentration = Duration.includes("Concentration")
-  spells.push({
-    "name": Name,
-    "desc": Description,
-    "page": "tcoe",
-    "range": Range,
-    "components": components,
-    "material": materials,
-    "duration": Duration,
-    "concentration": concentration ? "yes" : "no",
-    "ritual": Ritual ? "yes" : "no",
-    "school": School,
-    "level": levelStrs[Level],
-    "casting_time": CastingTime,
-    "class": Classes.join(", "),
-  })
+    const cantrip = Level == 0
+    const comp_bits = Components.split(" (")
+    const components = comp_bits[0]
+    const materials = comp_bits.length < 2 ? "" : comp_bits[1].split(")")[0]
+    const concentration = Duration.includes("Concentration")
+    spells.push({
+      "name": Name,
+      "desc": Description,
+      "page": page,
+      "range": Range,
+      "components": components,
+      "material": materials,
+      "duration": Duration,
+      "concentration": concentration ? "yes" : "no",
+      "ritual": Ritual ? "yes" : "no",
+      "school": School,
+      "level": levelStrs[Level] || parseInt(Level),
+      "casting_time": CastingTime,
+      "class": Classes.join(", "),
+    })
+  }
 }
+
+processDict(dataTCOE, "tcoe")
+processDict(dataEGTW, "egtw")
 
 export default spells

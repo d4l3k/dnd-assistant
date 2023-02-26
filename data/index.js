c391124cf0ffa45c84a54960ac1138c4cea09fa9
Tristan Rice <rice@fn.lc>
2023-02-25 16:15:16 -0800

    spells: added scc

--- data/index.js
+++ data/index.js
@@ -2,6 +2,7 @@ import dataSpells from './spellData.json'
 import dataDisciplines from './disciplines.json'
 import dataTCOE from './tcoe.json'
 import dataEGTW from './egtw.json'
+import dataSCC from './scc.json'
 
 const spells = dataSpells.concat(dataDisciplines)
 
@@ -51,7 +52,75 @@ function processDict (data, page) {
   }
 }
 
+function process5ETools(data) {
+
+  function renderList(entries, f, separator) {
+    console.log(entries)
+    const out = []
+    for (const entry of entries) {
+      out.push(f(entry))
+    }
+    if (!separator) {
+      separator = "\n"
+    }
+    return out.join(separator)
+  }
+
+  function renderEntry(entry) {
+    if (typeof entry === 'string') {
+      return entry
+    } else {
+      return entry["name"] + ": " + renderList(entry["entries"] || entry["items"], renderEntry)
+    }
+  }
+
+  function renderUnit(unit) {
+    return (unit["amount"] || unit["number"]) + " " + (unit["type"] || unit["unit"])
+  }
+
+  function renderDuration(duration) {
+    const type = duration["type"]
+    if (type == "timed") {
+      return renderUnit(duration["duration"])
+    }
+    return type
+  }
+
+  function renderRange(range) {
+    if (!range) {
+      return ""
+    }
+    return renderUnit(range["distance"]) + ", " + range["type"]
+  }
+
+  for (const spell of data["spell"]) {
+    let entries = []
+    if (spell["entries"]) {
+      entries = entries.concat(spell["entries"])
+    }
+    if (spell["entriesHigherLevel"]) {
+      entries = entries.concat(spell["entriesHigherLevel"])
+    }
+    spells.push({
+      "name": spell["name"],
+      "desc": renderList(entries, renderEntry),
+      "page": spell["source"] + " " + spell["page"],
+      "range": renderRange(data["range"]),
+      "components": Object.keys(spell["components"]).join(", "),
+      "material": spell["components"]["m"],
+      "duration": renderDuration(spell["duration"][0]),
+      "concentration": spell["duration"][0]["concentration"] ? "yes" : "no",
+      "ritual": spell["ritual"],
+      "school": spell["school"],
+      "level": spell["level"].toString(),
+      "casting_time": renderList(spell["time"], renderUnit, ", "),
+      "class": spell["class"]
+    })
+  }
+}
+
 processDict(dataTCOE, "tcoe")
 processDict(dataEGTW, "egtw")
+process5ETools(dataSCC)
 
 export default spells
